dbbd9e2b4afc2e6e91faa6e735a17ba7af3d39f3
aw2mgn <amoh8192@gmail.com>
2017-10-29 13:41:38 +0900

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,103 +1,68 @@
-;;===========================================
-;;  基本設定
-;;===========================================
-;;----
-;; elisp path
-;;----
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+;;;----
+;;; Basic settings
+;;;----
+;; elisp path and initialization package system
 (package-initialize)
-
 (setq load-path (cons "~/.emacs.d/elisp" load-path))
+(load (expand-file-name "~/.roswell/helper.el"))
 
-;;----
-;; UTF-8
-;;----
+;; set cording to UTF-8
 (prefer-coding-system 'utf-8)
 
-;;----
-;; スタートアップページを表示しない
-;;----
+;; don't show startup message
 (setq inhibit-startup-message t)
 
-;;----
-;; 行番号表示
-;;----
+;; show line and column number
 (global-linum-mode t)
 (setq linum-format "%3d ")
-
-;;----
-;; カラム番号
-;;----
 (column-number-mode t)
 
-;;----
-;; ビープ音を消す
-;;----
+;; don't sound ring-bell
 (setq ring-bell-function 'ignore)
 
-;;----
-;; カーソル行に下線を表示
-;;----
-;; 対応する括弧を強調表示
-;;----
+;; highlight parentheses
 (show-paren-mode t)
 
-;;----
 ;; indent settings
-;;----
 (setq-default tab-width 2 indent-tabs-mode nil)
 
-;;----
-;; ファイルサイズ表示
-;;----
+;; show file size
 (size-indication-mode t)
 
-;;----
-;; ツールバー,メニューバーを非表示
-;; M-x tool-bar-mode, M-x menu-bar-mode で表示非表示を切り替えられる
-;;----
+;; don't show tool and menu bar
 (if window-system (tool-bar-mode -1) (tool-bar-mode -1))
 (if window-system (menu-bar-mode -1) (menu-bar-mode -1))
 
-;;----
-;; タイトルバーにフルパス表示
-;;----
+;; show path on title bar
 (setq frame-title-format "%f")
 
-;;----
-;; カラーテーマ
-;;----
-(defun set-alpha (alpha-num)
-  "set frame parameter 'alpha"
-  (interactive "nAlpha: ")
-  (set-frame-parameter nil 'alpha (cons alpha-num '(90))))
-(set-alpha 90)
+;; color theme
 (load-theme 'wombat t)
-;;color-theme.el
-;(require 'color-theme)
-;(color-theme-initialize)
-;(color-theme-zenburn)
 
-;;----
-;; eww default search engine
-;;----
-(setq eww-search-prefix "https://www.google.co.jp/search?q=")
+;;autosave settings
+;;dont make save-list-file
+(setq auto-save-list-file-prefix nil)
+;;tmpolary file is maked on...
+(setq auto-save-file-name-transforms   '((".*" "~/tmp/" t)))
+;; save interval
+(setq auto-save-timeout 10)
+(setq auto-save-interval 100)
+;; backup is on "~/.ehist"
+(setq backup-directory-alist '((".*" . "~/.ehist")))
+;; save some file
+(setq version-control     t)
+(setq kept-new-versions   4)
+(setq kept-old-versions   1)
+(setq delete-old-versions t)
 
-;;===========================================
-;; キーボード操作系
-;;===========================================
-;;----
-;; バックスペースをC-hにする
-;;----
+
+;;;----
+;;; Keybinds
+;;;----
+;; set backspace to C-h
 (define-key global-map (kbd "C-h") 'delete-backward-char)
 
-;;----
 ;; window resizer
-;;----
 (defun window-resizer ()
   "Control window size and position."
   (interactive)
@@ -135,36 +100,9 @@
 (global-set-key "\C-qj" 'windmove-down)
 (global-set-key "\C-qk" 'windmove-up)
 
-
-;;----
-;; window move (shift-pointerkey)
-;;----
-(when (fboundp 'windmove-default-keybindings)
-  (windmove-default-keybindings))
-
-;;===========================================
-;; オートセーブ系
-;;===========================================
-;;configure of autosavefile
-;;dont make save-list-file
-(setq auto-save-list-file-prefix nil)
-;;tmpolary file is maked on...
-(setq auto-save-file-name-transforms   '((".*" "~/tmp/" t)))
-;; 保存の間隔
-(setq auto-save-timeout 10)     ;; 秒   (デフォルト : 30)
-(setq auto-save-interval 100)   ;; 打鍵 (デフォルト : 300)
-;;backup file
-(setq backup-directory-alist '((".*" . "~/.ehist")))
-;; 番号付けによる複数保存
-(setq version-control     t)  ;; 実行の有無
-(setq kept-new-versions   4)  ;; 最新の保持数
-(setq kept-old-versions   1)  ;; 最古の保持数
-(setq delete-old-versions t)  ;; 範囲外を削除
-
-
-;;=============================================
-;; Scheme-mode
-;;=============================================
+;;;----
+;;; Scheme-mode
+;;;----
 (setq scheme-program-name "gosh -i")
 (autoload 'scheme-mode "cmuscheme" "Major mode for Scheme." t)
 (autoload 'scheme-mode "cmuscheme" "Run an inferior Scheme process." t)
@@ -175,17 +113,3 @@
         (get-buffer-create "*scheme*"))
     (run-scheme scheme-program-name))
 (define-key global-map "\C-cs" 'scheme-other-window)
-
-
-;;===============================================
-;; Common-Lisp
-;;===============================================
-;(setq inferior-lisp-program "sbcl")
-;(add-to-list 'load-path (expand-file-name "~/.emacs.d/elisp/slime"))
-;(require 'slime)
-;(slime-setup '(slime-repl slime-fancy slime-banner))
-
-;;===============================================
-;; roswell
-;;===============================================
-(load (expand-file-name "~/.roswell/helper.el"))
--- .vimrc
+++ .vimrc
@@ -1,4 +1,3 @@
-"minimum .vimrc
 syntax enable
 
 set number
@@ -14,119 +13,9 @@ set nowrapscan
 set ignorecase
 set smartcase
 set hidden
-set history=2000
+set history=1024
 set autoindent
-set helplang=ja
-set wrap
-
-nnoremap <Space>w  :<C-u>w<CR>
-nnoremap <Space>q  :<C-u>q<CR>
-nnoremap <Space>Q  :<C-u>q!<CR>
-
-nnoremap ;  :
-nnoremap :  ;
-vnoremap ;  :
-vnoremap :  ;
-
-nnoremap <Space>h  ^
-nnoremap <Space>l  $
-
-nnoremap k   gk
-nnoremap j   gj
-vnoremap k   gk
-vnoremap j   gj
-nnoremap gk  k
-nnoremap gj  j
-vnoremap gk  k
-vnoremap gj  j
-
-nnoremap <Space>/  *<C-o>
-nnoremap g<Space>/ g*<C-o>
-
-nnoremap <expr> n <SID>search_forward_p() ? 'nzv' : 'Nzv'
-nnoremap <expr> N <SID>search_forward_p() ? 'Nzv' : 'nzv'
-vnoremap <expr> n <SID>search_forward_p() ? 'nzv' : 'Nzv'
-vnoremap <expr> N <SID>search_forward_p() ? 'Nzv' : 'nzv'
-
-function! s:search_forward_p()
-    return exists('v:searchforward') ? v:searchforward : 1
-endfunction
-
-nnoremap <Space>o  :<C-u>for i in range(v:count1) \| call append(line('.'), '') \| endfor<CR>
-nnoremap <Space>O  :<C-u>for i in range(v:count1) \| call append(line('.')-1, '') \| endfor<CR>
-
-nnoremap <silent> tt  :<C-u>tabe<CR>
-nnoremap <C-p>  gT
-nnoremap <C-n>  gt
-
-nnoremap <silent> <Esc><Esc> :<C-u>nohlsearch<CR>
-
-nnoremap ZZ <Nop>
-nnoremap ZQ <Nop>
-
-nnoremap Q gq
-
-onoremap aa  a>
-onoremap ia  i>
-onoremap ar  a]
-onoremap ir  i]
-onoremap ad  a"
-onoremap id  i"
-
-inoremap jk  <Esc>
-
-nnoremap gs  :<C-u>%s///g<Left><Left><Left>
-vnoremap gs  :s///g<Left><Left><Left>
-
-cnoremap <C-f>  <Right>
-cnoremap <C-b>  <Left>
-cnoremap <C-a>  <C-b>
-cnoremap <C-e>  <C-e>
-cnoremap <C-u> <C-e><C-u>
-cnoremap <C-v> <C-f>a
-
-"tab
-set tabstop=4
-set shiftwidth=4
 set expandtab
-set softtabstop=0
-
-"split
-nnoremap s <Nop>
-nnoremap sj <C-w>j
-nnoremap sk <C-w>k
-nnoremap sl <C-w>l
-nnoremap sh <C-w>h
-nnoremap sJ <C-w>J
-nnoremap sK <C-w>K
-nnoremap sL <C-w>L
-nnoremap sH <C-w>H
-nnoremap sn gt
-nnoremap sp gT
-nnoremap sr <C-w>r
-nnoremap s= <C-w>=
-nnoremap sw <C-w>w
-nnoremap so <C-w>_<C-w>|
-nnoremap sO <C-w>=
-nnoremap sN :<C-u>bn<CR>
-nnoremap sP :<C-u>bp<CR>
-nnoremap st :<C-u>tabnew<CR>
-nnoremap ss :<C-u>sp<CR>
-nnoremap sv :<C-u>vs<CR>
-nnoremap sq :<C-u>q<CR>
-nnoremap sQ :<C-u>bd<CR>
-
-"colorscheme
-set t_Co=256
-"colorscheme jellybeans
-"transparency background
-if !has('gui_running')
-	augroup background
-		autocmd!
-		autocmd VimEnter,ColorScheme * highlight Normal ctermbg=none
-		autocmd VimEnter,ColorScheme * highlight LineNr ctermbg=none
-		autocmd VimEnter,ColorScheme * highlight SignColumn ctermbg=none
-		autocmd VimEnter,ColorScheme * highlight VertSplit ctermbg=none
-		autocmd VimEnter,ColorScheme * highlight NonText ctermbg=none
-	augroup END
-endif
+set tabstop=2
+set shiftwidth=2
+set helplang=en
