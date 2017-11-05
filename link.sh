16c8ca72e50072815c2860f040d16ac15c4b897b
aw2mgn <amoh8192@gmail.com>
2017-11-05 15:06:35 +0900

    change xmodmap

--- link.sh
+++ link.sh
@@ -4,5 +4,5 @@ ln -sf ~/dotfiles/.emacs.d/init.el ~/.emacs.d/init.el
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.xinitrc ~/.xinitrc
 ln -sf ~/dotfiles/.Xresources ~/.Xresources
-ln -sf ~/dotfiles/.xmodmap ~/.xmodmap
+ln -sf ~/dotfiles/.Xmodmap ~/.Xmodmap
 ln -sf ~/dotfiles/.config/i3/config ~/.config/i3/config
